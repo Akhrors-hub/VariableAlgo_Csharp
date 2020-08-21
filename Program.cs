c1d8a3e8e0d2eb305140883e21f1769cb529873b
Ahrorbek <mi7akhror@gmail.com>
2020-08-21 11:26:48 -0700

    pushing the changes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariableAlgo_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
